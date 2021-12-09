e9237a06984856358d0e30acb08c53e3e3fba1a7
Alexiu Adrian <adi.alexiu@gmail.com>
2021-12-09 21:02:36 +0200

    configured cors for production

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001'
+    origins 'https://roadrunner-fe.herokuapp.com'
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-             credentials: true
+      credentials: true
   end
 end
